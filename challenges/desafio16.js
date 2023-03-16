4ae5cd132d9b730951bf02167d990bf56a369f9e
PedroJoMa <pejomadejefi@gmail.com>
2023-03-16 18:18:20 -0300

    desafio 16 concluído

---
+++ challenges/desafio16.js
@@ -0,0 +1,11 @@
+db.produtos.updateMany({
+  nome: { $eq: "Big Mac" },
+}, {
+  $set: { ultimaModificacao: { type: "timestamp" } },
+});
+db.produtos.find({
+  ultimaModificacao: { $exists: true },
+}, {
+  nome: true,
+  _id: false,
+});
