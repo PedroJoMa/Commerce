9eb7ae360670d3767917352d3ba5c90e91ba3d1a
PedroJoMa <pejomadejefi@gmail.com>
2023-03-17 19:56:51 -0300

    desafio 17 concluído

---
+++ challenges/desafio17.js
@@ -0,0 +1,9 @@
+db.resumoProdutos.insertOne({
+  franquia: "McDonalds",
+  totalProdutos: db.produtos.countDocuments({}),
+});
+db.resumoProdutos.find({}, {
+  _id: false,
+  franquia: true,
+  totalProdutos: true,
+});
