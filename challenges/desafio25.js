c5cd8dd6e74b75e854237dc1cc00ab650e9aadb1
PedroJoMa <pejomadejefi@gmail.com>
2023-03-19 01:04:43 -0300

    desafio 25 concluído

---
+++ challenges/desafio25.js
@@ -0,0 +1,14 @@
+db.produtos.updateMany({
+  "valoresNutricionais.2.percentual": {
+    $gte: 40,
+  },
+}, {
+  $push: {
+    tags: "muito sódio",
+  },
+});
+db.produtos.find({}, {
+  nome: true,
+  tags: true,
+  _id: false,
+});
